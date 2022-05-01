a0183adff6e813a37209df969513609aca63b106
Haydar Alçin <hydralcin@gmail.com>
2022-05-01 18:27:43 +0300

    Hafıza Oyunu
    C# MegaBrain

---
+++ Ecodation_Hafiza_Oyunu/Ecodation_Hafiza_Oyunu/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace Ecodation_Hafiza_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(765, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 24);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "0";
+            // 
+            // button14
+            // 
+            this.button14.BackgroundImage = global::Ecodation_Hafiza_Oyunu.Properties.Resources.bomb;
+            this.button14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button14.Location = new System.Drawing.Point(393, 398);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(111, 97);
+            this.button14.TabIndex = 13;
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackgroundImage = global::Ecodation_Hafiza_Oyunu.Properties.Resources.bomb;
+            this.button13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button13.Location = new System.Drawing.Point(247, 398);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(111, 97);
+            this.button13.TabIndex = 12;
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackgroundImage = global::Ecodation_Hafiza_Oyunu.Properties.Resources.bomb;
+            this.button12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button12.Location = new System.Drawing.Point(539, 295);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(111, 97);
+            this.button12.TabIndex = 11;
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackgroundImage = global::Ecodation_Hafiza_Oyunu.Properties.Resources.bomb;
+            this.button11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button11.Location = new System.Drawing.Point(393, 295);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(111, 97);
+            this.button11.TabIndex = 10;
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackgroundImage = global::Ecodation_Hafiza_Oyunu.Properties.Resources.bomb;
+            this.button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button10.Location = new System.Drawing.Point(247, 295);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(111, 97);
+            this.button10.TabIndex = 9;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackgroundImage = global::Ecodation_Hafiza_Oyunu.Properties.Resources.bomb;
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button9.Location = new System.Drawing.Point(101, 295);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(111, 97);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = global::Ecodation_Hafiza_Oyunu.Properties.Resources.bomb;
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button8.Location = new System.Drawing.Point(539, 192);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(111, 97);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = global::Ecodation_Hafiza_Oyunu.Properties.Resources.bomb;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button7.Location = new System.Drawing.Point(393, 192);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(111, 97);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::Ecodation_Hafiza_Oyunu.Properties.Resources.bomb;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.Location = new System.Drawing.Point(247, 192);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(111, 97);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::Ecodation_Hafiza_Oyunu.Properties.Resources.bomb;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.Location = new System.Drawing.Point(101, 192);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 97);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::Ecodation_Hafiza_Oyunu.Properties.Resources.bomb;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(539, 89);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 97);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::Ecodation_Hafiza_Oyunu.Properties.Resources.bomb;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(393, 89);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 97);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::Ecodation_Hafiza_Oyunu.Properties.Resources.bomb;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(247, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 97);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Ecodation_Hafiza_Oyunu.Properties.Resources.bomb;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(101, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 97);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(797, 601);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Ecodation_Hafiza_Oyunu/Ecodation_Hafiza_Oyunu/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ecodation_Hafiza_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        Image defult_foto = Properties.Resources.bomb;
+
+        Image[] resimler = 
+            {
+                 Properties.Resources.number_one,
+                 Properties.Resources.number_2,
+                 Properties.Resources.number_3,
+                 Properties.Resources.number_four,
+                 Properties.Resources.number_5,
+                 Properties.Resources.six,
+                 Properties.Resources.seven,
+            };
+        int[] indeks = {0,0,1,1,2,2,3,3,4,4,5,5,6,6 };
+        int sayac = 0;
+        bool once_tiklanan_varmi=false;
+        Button once_tiklanan_buton;
+        int once_tiklanan_buton_id;
+
+        public void resimleri_karistir()
+        {
+            Random r = new Random();
+            for (int i = 0; i < indeks.Length; i++)
+            {
+                int j = r.Next(14);
+                int tutucu = indeks[i];
+                indeks[i] = indeks[j];
+                indeks[j] = tutucu;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resimleri_karistir();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayac++;
+            label1.Text = Convert.ToString(sayac);
+            Button click_edilen_buton = (Button)sender;
+            int button_id = Convert.ToInt32(click_edilen_buton.Name.Substring(6))-1;
+
+            if (once_tiklanan_varmi==false)
+            {
+                click_edilen_buton.Image = resimler[indeks[button_id]];
+                once_tiklanan_varmi = true;
+                click_edilen_buton.Enabled = false;
+                once_tiklanan_buton = click_edilen_buton;
+                once_tiklanan_buton_id = button_id;
+            }
+            else
+            {
+                click_edilen_buton.Image = resimler[indeks[button_id]];
+                once_tiklanan_varmi = false;
+                click_edilen_buton.Enabled = false;
+                if (click_edilen_buton.Image!=once_tiklanan_buton.Image)
+                {
+                    System.Threading.Thread.Sleep(1000); //Resim 1 Saniye Kalıyor Görmemiz İçin Sonra Aşağıya Geçiyoruz
+
+                    click_edilen_buton.Image = defult_foto;
+                    once_tiklanan_buton.Image = defult_foto;
+                    once_tiklanan_buton.Enabled = true;
+                    click_edilen_buton.Enabled = true;
+                }
+            }
+        }
+    }
+}
